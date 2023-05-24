c10fb4b4691ba9aa8bc2fab2b429d9bf4805e248
EchtkPvL <EchtkPvL@users.noreply.github.com>
2023-05-24 07:24:57 +0200

    update

--- World/Door.gd
+++ World/Door.gd
@@ -12,18 +12,26 @@ func go_to_next_room():
 	get_tree().paused = false
 	Global.high_score[Global.current_level] = Global.current_score
 	get_tree().change_scene(target_level_path)
+	queue_free()
 
 func _process(delta):
-	if not player: return
-	if not player.is_on_floor(): return
-	if Input.is_action_just_pressed("ui_up"):
-		if target_level_path.empty(): return
-		go_to_next_room()
+	pass
+#	if not player: return
+#	if not player.is_on_floor(): return
+##	if Input.is_action_just_pressed("ui_up"):
+#	if target_level_path.empty(): return
+#	go_to_next_room()
+
 
 func _on_Door_body_entered(body):
 	if not body is Player: return
 	body.on_door = true
 	player = body
+	if not player: return
+	if not player.is_on_floor(): return
+#	if Input.is_action_just_pressed("ui_up"):
+	if target_level_path.empty(): return
+	go_to_next_room()
 
 func _on_Door_body_exited(body):
 	if not body is Player: return
