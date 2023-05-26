d6e9acc7980d5058da69857ee8f8c4f1dc5473a5
EchtkPvL <info@jonas-berner.de>
2023-05-26 22:37:07 +0200

    update

--- World/Door.gd
+++ World/Door.gd
@@ -14,27 +14,26 @@ func go_to_next_room():
 	get_tree().change_scene(target_level_path)
 	queue_free()
 
-func _process(delta):
-	pass
-#	if not player: return
-#	if not player.is_on_floor(): return
-##	if Input.is_action_just_pressed("ui_up"):
-#	if target_level_path.empty(): return
-#	go_to_next_room()
+#func _process(delta):
+#	pass
+##	if not player: return
+##	if not player.is_on_floor(): return
+###	if Input.is_action_just_pressed("ui_up"):
+##	if target_level_path.empty(): return
+##	go_to_next_room()
 
 
 func _on_Door_body_entered(body):
 	if not body is Player: return
-	body.on_door = true
+#	body.on_door = true
 	player = body
 	if not player: return
-	if not player.is_on_floor(): return
-#	if Input.is_action_just_pressed("ui_up"):
+#	if not player.is_on_floor(): return
 	if target_level_path.empty(): return
 	go_to_next_room()
 
 func _on_Door_body_exited(body):
-	if not body is Player: return
-	body.on_door = false
-	player = null
-	
+	pass
+#	if not body is Player: return
+#	body.on_door = false
+#	player = null
